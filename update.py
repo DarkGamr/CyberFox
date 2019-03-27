15380360db25ae5e39326491aaced858e7c4e72f
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-03-28 00:07:48 +0200

    Update update.py

--- update.py
+++ update.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 os.system("rm -rf CyberFox")
-os.system("git clone" https://github.com/DarkGa/CyberFox")
+os.system("git clone https://github.com/DarkGa/CyberFox")
 os.system("cd $HOME && rm -rf update.py")
