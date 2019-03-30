02c1aa6fd40b2a157514b623ad2ee46d8fe88d88
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-03-30 21:52:29 +0000

    Add files via upload

---
+++ ядро/help.py
@@ -0,0 +1,7 @@
+from core.colors import *
+print(Y+"modules:"+W)
+print("1. use modules qiwi/hacker")
+print("2. [fast downloader] a new version!")
+print(DC+"exet"+W)
+print("1. quit")
+van = input('')
