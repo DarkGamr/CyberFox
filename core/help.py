bdb30877899dcb6006aa2a8b138fef456c685f1c
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-04-12 23:07:59 +0300

    Add files via upload

---
+++ core/help.py
@@ -0,0 +1,12 @@
+from core.colors import *
+import time
+print(Y+"★-★-★-★-★-★-★-★"+W)
+print(Y+"|m|o|d|u|l|e|s|:"+W)
+print(Y+"★-★-★-★-★-★-★-★"+W)
+print("1. use modules qiwi/hacker")
+print("2. [fast downloader] a new version!")
+print(DC+"★-★-★-★-★"+W)
+print(DC+"|e|x|e|t|"+W)
+print(DC+"★-★-★-★-★"+W)
+print("1. quit")
+time.sleep(7)
